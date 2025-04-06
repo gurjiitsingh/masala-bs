ccae4ba0d41e4d92f0f71e971ad92e7320fe1cde
gurnit <gurjiitsingh@gmail.com>
2025-04-06 10:40:05 +0530

    responsive change

--- src/app/(home)/Components/PageProductDetailComponent.tsx
+++ src/app/(home)/Components/PageProductDetailComponent.tsx
@@ -40,7 +40,7 @@ export default function PageProductDetailComponent({
     image: product.image,
   };
   return (
-    <div className="w-full  md:w-[48%]   bg-zinc-50 shadow-lg flex flex-row   rounded-2xl items-center">
+    <div className="w-full  lg:w-[48%]   bg-zinc-50 shadow-lg flex flex-row   rounded-2xl items-center">
       <div className="rounded-full flex items-center justify-center w-[70px] h-[65px]  md:w-[90px]  md:h-[80px]  overflow-hidden">
         <img src={product.image} className="h-[65px]  md:h-[85px]" />
       </div>
