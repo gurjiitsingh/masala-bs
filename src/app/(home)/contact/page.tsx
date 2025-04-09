419907287ee5f961889f3a94b26b966a2baca27e
gurnit <gurjiitsingh@gmail.com>
2025-04-09 16:46:25 +0530

    contact page update

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,66 +1,73 @@
 import React from 'react'
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa'
 
 export default function Contact() {
   return (
-    <div className="container mx-auto py-5 p-1">
-      
-      <div className="flex flex-col gap-8 md:flex-row my-24 justify-between">
-        {/* Address Section */}
-        <div className=" flex flex-col">
-          <h1 className="text-[#333] text-[3rem]">Masala Taste of India</h1>
-          <div className="w-full md:w-[50%] space-y-3 text-lg">
-            <a className="relative mb-5 block aspect-square w-20" href="/de">
-              <img
-                alt="Pizzabaaz"
-                src="https://assets-prod-gillz.s3.eu-central-1.amazonaws.com/e251843b-6f27-458d-9d6f-0bc164920168/1731781573178_kxSGE1X.png"
-                width={80}
-                height={80}
-                className="object-contain"
-              />
-            </a>
-            <a
-              className="flex items-center justify-start"
-              title="Braunschweiger Str. 93, 38518 Gifhorn, Germany"
-              href="http://maps.google.com/maps?q=52.4717066,10.5434431"
-              target="_blank"
-              rel="noreferrer"
-            >
-              📍 Braunschweiger Str. 93, 38518 Gifhorn, Germany
-            </a>
-            <a
-              className="flex items-center justify-start"
-              title="05371 6266291"
-              href="tel:053716266291"
-              target="_blank"
-              rel="noreferrer"
-            >
-              📞 05371 6266291
-            </a>
-            <a
-              className="flex items-center justify-start"
-              title="info@masala-gf.de"
-              href="mailto:info@masala-gf.de"
-              target="_blank"
-              rel="noreferrer"
-            >
-              ✉️ info@masala-gf.de
-            </a>
-          </div>
-        </div>
+    <div className="container mx-auto px-4">
+    <div className="flex flex-col md:flex-row my-24 gap-8">
+      {/* Address Info */}
+      <div className="flex flex-col md:w-1/2">
+        <h1 className="text-[#333] text-5xl mb-6">Masala Taste of India</h1>
+        <div className="space-y-4 text-lg">
+          <a className="relative block aspect-square w-20 mb-5" href="/de">
+            <img
+              src="https://assets-prod-gillz.s3.eu-central-1.amazonaws.com/e251843b-6f27-458d-9d6f-0bc164920168/1731794259221_v6Yv4Ez.png"
+              alt="Masala Logo"
+              width={80}
+              height={80}
+              className="object-contain"
+            />
+          </a>
 
-        {/* Google Map Section */}
-        <div className="google_map w-full md:w-[50%]">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2430.4102913899947!2d10.540868176678202!3d52.47170657204857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDI4JzE4LjEiTiAxMMKwMzInMzYuNCJF!5e0!3m2!1sen!2sin!4v1736146998436!5m2!1sen!2sin"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-           
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <a
+            className="flex items-center"
+            href="http://maps.google.com/maps?q=52.2368464,10.4778527"
+            target="_blank"
+            rel="noreferrer"
+            title="Steinbrink 26, 38122 Braunschweig-Broitzem, Germany"
+          >
+            <FaMapMarkerAlt className="text-gray-600 mr-2" />
+            Steinbrink 26, 38122 Braunschweig-Broitzem, Germany
+          </a>
+
+          <a
+            className="flex items-center"
+            href="tel:053138963516"
+            target="_blank"
+            rel="noreferrer"
+            title="053138963516"
+          >
+            <FaPhoneAlt className="text-gray-600 mr-2" />
+            053138963516
+          </a>
+
+          <a
+            className="flex items-center"
+            href="mailto:info@masala-gf.de"
+            target="_blank"
+            rel="noreferrer"
+            title="info@masala-gf.de"
+          >
+            <FaEnvelope className="text-gray-600 mr-2" />
+            info@masala-gf.de
+          </a>
         </div>
       </div>
+
+      {/* Google Map */}
+      <div className="w-full md:w-1/2">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2443.3598947836963!2d10.47527777666399!3d52.23684637198838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDE0JzEyLjciTiAxMMKwMjgnNDAuMyJF!5e0!3m2!1sen!2sin!4v1736048462114!5m2!1sen!2sin"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full"
+        ></iframe>
+      </div>
     </div>
+  </div>
   )
 }
